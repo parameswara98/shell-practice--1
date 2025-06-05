fb7300d919ebc73eef16c339ca4c0da513d026de
parameswara98 <parameswarareddy98@gmail.com>
2025-06-05 16:31:33 +0530

    modofied

--- 10- install-commands.sh
+++ 10- install-commands.sh
@@ -9,6 +9,11 @@ exit 1 # give other than 0 upto 127
 else
 echo " you are running with the root access"
 fi 
+dnf list installed mysql 
+
+if [ $? -ne 0 ]
+then 
+echo "my sql is not installed ...going to install "
 dnf install mysql -y 
 
 if [ $? -eq 0 ]
@@ -18,3 +23,19 @@ else
 echo " Mysql is failed to install"
 exit 1
 fi 
+else 
+echo " mysql is already installed nothing to do "
+fi 
+
+# if [ $? -eq 0 ]
+# then 
+# echo " My sql is installed succesfully "
+# else
+# echo " Mysql is failed to install"
+# exit 1
+# fi 
+
+# dnf list installed mysql 
+#echo $?
+# dnf list installed nginx
+#echo $?
