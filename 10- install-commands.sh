428eaa83562611438c714f7618704c12e5f442ef
parameswara98 <parameswarareddy98@gmail.com>
2025-06-05 16:12:54 +0530

    modofied

--- 10- install-commands.sh
+++ 10- install-commands.sh
@@ -5,7 +5,16 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
 echo " Error : please run this script using the root access "
+exit 1 # give other than 0 upto 127
 else
 echo " you are running with the root access"
 fi 
-dnf install mysql -y 
+dnf install mysq -y 
+
+if [ $? -eq 0 ]
+then 
+echo " My sql is installed succesfully "
+else
+echo " Mysql is failed to install"
+exit 1
+fi 
