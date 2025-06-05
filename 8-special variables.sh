09dbde875bdc48beeec7ddb73691bce8345bc67d
parameswara98 <parameswarareddy98@gmail.com>
2025-06-05 09:03:14 +0530

    modofied

---
+++ 8-special variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "All the variables passed : $@ "
+echo " number of arguments passed : $# "
+echo " script name : $0 "
+echo " present working directory : $PWD"
+echo " home directory of the user : $HOME "
+echo " which user is running ther script : $USER"
+echo " Process ID of the Script : $PID"
+sleep 10 &  
+echo " process runnning in the background : $!"
+
