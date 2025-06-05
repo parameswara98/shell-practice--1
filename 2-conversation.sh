a0585b1d495b5149e86432b28424f59c1a11f28d
parameswara98 <parameswarareddy98@gmail.com>
2025-06-05 07:33:41 +0530

    modofied

---
+++ 2-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Trump: Hey Musk, How are you ?"
+echo "Musk: Hello Trump, I am fine, How are you doing?"
+echo  "Trump : Ii am fine too. whats up?"
+echo "Musk: Nothing ,just going to mars now, will you come"
+echo  "Trump: soory, you carry on! I will come once you are back ."
