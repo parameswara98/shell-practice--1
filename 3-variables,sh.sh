170b5cd0fec8858fdaed563a58f70ef31527c5fb
parameswara98 <parameswarareddy98@gmail.com>
2025-06-05 07:48:24 +0530

    modofied

---
+++ 3-variables,sh.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1=Trump
+person2=Musk
+echo "$person1: Hey $person2, How are you ?"
+echo "$person2: Hello $person1, I am fine, How are you doing?"
+echo  "$person1 : Ii am fine too. whats up?"
+echo "$person2: Nothing ,just going to mars now, will you come"
+echo  "$person1: soory, you carry on! I will come once you are back ."
